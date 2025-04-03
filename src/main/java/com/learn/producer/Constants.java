477d0bc9e9471ee55de43fe270b5f33ad8be4c5f
shubhamk1838 <jadoo.8k@gmail.com>
2025-04-03 12:02:27 +0000

    producer endpoint...

---
+++ src/main/java/com/learn/producer/Constants.java
@@ -0,0 +1,7 @@
+package com.learn.producer;
+
+public class Constants {
+
+    public static final String TOPIC = "user-topic";
+
+}
---
+++ src/main/java/com/learn/producer/constant/Constants.java
@@ -0,0 +1,6 @@
+package com.learn.producer.constant;
+
+public class Constants {
+
+    public static final String TOPIC_NAME = "user-topic";
+}
